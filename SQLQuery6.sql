2e8bc642aad6e239ccd164a15d54bbd3de1a1dc1
KatlegoLefenya <61087544+KatlegoLefenya@users.noreply.github.com>
2024-06-18 23:04:34 +0200

    Add files via upload

---
+++ SQLQuery6.sql
@@ -0,0 +1,6 @@
+insert into status values (1,'Resolved','Problem was solved')
+insert into status values (2,'Referred','Customer was referred to the relevent companys representative')
+insert into status values (3,'Declined','Problem was not solved due to customers fault')
+insert into status values (4,'Unresolved','Problem was not solved due to companys fault')
+
+select * from status
